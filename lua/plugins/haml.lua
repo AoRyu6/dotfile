c2a1aecaf1683508141dceec691997d5a80b0860
AoRyu6 <ryu17615@gmail.com>
2024-12-30 01:53:49 +0900

    feat: backup

--- lua/plugins/haml.lua
+++ lua/plugins/haml.lua
@@ -4,17 +4,23 @@ return {
     config = function()
       -- Configuration goes here.
       local g = vim.g
+
+      -- HAMLのみにhamllintを適用
       g.ale_linters = {
         haml = { "hamllint" },
       }
-      g.ale_lint_on_save = 1
-      g.ale_set_balloons = 1
-      g.ale_virtualtext_cursor = 1
-      g.ale_virtualtext = 1
 
-      g.ale_virtualtext = 1 -- 仮想テキストを有効化
-      g.ale_virtualtext_cursor = 1 -- カーソル位置のエラーも表示
-      g.ale_set_balloons = 1 -- エラーをポップアップで表示
+      -- ファイルタイプがhamlのときにALEを動作させる
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "haml", -- ここでファイルタイプを指定
+        callback = function()
+          -- エラー表示の設定
+          g.ale_lint_on_save = 0
+          g.ale_set_balloons = 0
+          g.ale_virtualtext_cursor = 0
+          g.ale_virtualtext = 1
+        end,
+      })
     end,
   },
 }
