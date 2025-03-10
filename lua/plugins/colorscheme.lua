bf683181db5ea2fb2baca6c3bd8dd1ef382818b4
AoRyu6 <ryu17615@gmail.com>
2025-03-10 14:50:02 +0900

    chore: backup

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,4 @@
 return {
-  -- "f-person/auto-dark-mode.nvim",
-  -- opts = {
-  --   update_interval = 1000,
-  --   set_dark_mode = function()
-  --     vim.api.nvim_set_option_value("background", "dark", {})
-  --     vim.cmd("colorscheme catppuccin-frappe")
-  --   end,
-  --   set_light_mode = function()
-  --     vim.api.nvim_set_option_value("background", "light", {})
-  --     vim.cmd("colorscheme dracula")
-  --   end,
-  -- },
 
   { "killitar/obscure.nvim" },
   {
@@ -19,11 +7,6 @@ return {
       "tjdevries/colorbuddy.nvim",
     },
   },
-  {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-  },
   { "olivercederborg/poimandres.nvim" },
   { "Mofiqul/dracula.nvim" },
   { "rose-pine/neovim", name = "rose-pine" },
@@ -31,22 +14,11 @@ return {
   { "EdenEast/nightfox.nvim" },
   { "rmehri01/onenord.nvim" },
   { "sainnhe/sonokai" },
-  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
-  { "projekt0n/github-nvim-theme", name = "github-theme" },
-  { "xiantang/darcula-dark.nvim" },
-  {
-    "craftzdog/solarized-osaka.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      transparent = true,
-    },
-  },
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "industry",
-      colorscheme = "vitesse",
+      colorscheme = "rose-pine-moon",
       -- colorscheme = "carbonfox",
     },
   },
