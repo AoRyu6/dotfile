78f04307875672fbf46ce88b7fb9f9a692a14a4c
Ao65 <ryu17615@gmail.com>
2025-03-17 20:00:07 +0900

    chore: backup

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,25 +1,20 @@
 return {
 
-  { "killitar/obscure.nvim" },
   {
     "2nthony/vitesse.nvim",
     dependencies = {
       "tjdevries/colorbuddy.nvim",
     },
   },
-  { "olivercederborg/poimandres.nvim" },
   { "Mofiqul/dracula.nvim" },
   { "rose-pine/neovim", name = "rose-pine" },
   { "olivercederborg/poimandres.nvim" },
   { "EdenEast/nightfox.nvim" },
   { "rmehri01/onenord.nvim" },
-  { "sainnhe/sonokai" },
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "industry",
-      colorscheme = "rose-pine-moon",
-      -- colorscheme = "carbonfox",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,10 +1,3 @@
 return {
   { "xiyaowong/transparent.nvim" },
-  { "adelarsq/image_preview.nvim" },
-  -- {
-  --   "olrtg/nvim-emmet",
-  --   config = function()
-  --     vim.keymap.set({ "n", "v" }, "<leader>xe", require("nvim-emmet").wrap_with_abbreviation)
-  --   end,
-  -- },
 }
